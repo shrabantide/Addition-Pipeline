00b4d05e10e3cbecf2a3a7d37a4e651f7c8638ea
shrabantide <118583893+shrabantide@users.noreply.github.com>
2026-03-30 10:04:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ def add(a, b):
 
 @app.route("/")
 def home():
-    return f"Result: {add(8,2)}"
+    return "Hello DevOps"
 
+@app.route("/add/<int:a>/<int:b>")
+def addition(a, b):
+    return str(add(a, b))
+
+# IMPORTANT 👇
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
